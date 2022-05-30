6e7f6fec0d152216155bea98a373cdabc3e39587
ratamon-tech <ratamon91@gmail.com>
2022-05-30 21:55:06 +0900

    タスク追加エンドポイント作成

--- src/main/java/jp/ratamon/pro/java/tasklist/HomeRestController.java
+++ src/main/java/jp/ratamon/pro/java/tasklist/HomeRestController.java
@@ -1,10 +1,13 @@
 package jp.ratamon.pro.java.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
@@ -18,5 +21,15 @@ public class HomeRestController {
                 It works!
                 現在時刻は%sです。
                 """.formatted(LocalDateTime.now());
+    }
+
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました。";
     }
 }
