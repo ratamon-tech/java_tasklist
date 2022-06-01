7731574bac954d64ae3a3742b71017e58493dd33
ratamon-tech <ratamon91@gmail.com>
2022-06-01 21:55:08 +0900

    HomeController追加

---
+++ src/main/java/jp/ratamon/pro/java/tasklist/HomeController.java
@@ -0,0 +1,25 @@
+package jp.ratamon.pro.java.tasklist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/hello")
+    @ResponseBody
+    String hello() {
+        return """
+                <html>
+                    <head><title>Hello</title></head>
+                    <body>
+                        <h1>Hello</h1>
+                        It works!<br>
+                        現時刻は%sです。
+                    </body>
+                </html>
+                """.formatted(LocalDateTime.now());
+    }
+}
