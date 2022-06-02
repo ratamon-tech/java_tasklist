8a6f3b27d46c171e85120e59c6c2cc0852a81d03
ratamon-tech <ratamon91@gmail.com>
2022-06-03 08:28:18 +0900

    タスク管理アプリケーションIF実装

--- src/main/java/jp/ratamon/pro/java/tasklist/HomeController.java
+++ src/main/java/jp/ratamon/pro/java/tasklist/HomeController.java
@@ -1,25 +1,29 @@
 package jp.ratamon.pro.java.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem>  taskItems = new ArrayList<>();
     @RequestMapping(value="/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                        <h1>Hello</h1>
-                        It works!<br>
-                        現時刻は%sです。
-                    </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
+    }
+
+    @GetMapping("/list")
+    String listItems(Model model) {
+        model.addAttribute("taskList", taskItems);
+        return "home";
     }
 }
